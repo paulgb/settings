3b5bce9fbd23190259ae42257bd52df6ff4bcb4b
Paul Butler <paulgb@gmail.com>
2015-08-05 23:22:29 -0400

    Update remote_install.sh

--- remote_install.sh
+++ remote_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/paulgb/settings.git ~/.settings
+cd ~/
+git clone https://github.com/paulgb/settings.git .settings
 cd ~/.settings/
 bash ~/.settings/install.sh
 
